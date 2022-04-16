783ed3620f3b329778b28903e660b64d2e9ad8a9
Junyong <jypark93@kookmin.ac.kr>
2022-04-16 13:31:23 +0900

    add:그룹단어체커

--- algorithm/silver/Silver5/그룹단어체커.java
+++ algorithm/silver/Silver5/그룹단어체커.java
@@ -7,28 +7,28 @@ class Main {
     int cnt = 0;
     testCase = sc.nextInt();
     int temp =testCase;
-    while(testCase!=0){
-        int alpha[] = new int [27];
-        String str ;
-        str = sc.nextLine();
+    while(temp!=0){
+        int alpha[] = new int [26]; // 알파벳을 체크하기 위한 알파벳 변수
+        String str = sc.next(); 
         int len = str.length(); 
-        check = 0;
+        check = 0; //연속된 문자를 확인하기 위한 변수
         for(int i=0; i<len; i++){
-            if(alpha[str.charAt(i)-97]==0){   
-                 alpha[str.charAt(i)] = 1;
-                 check = str.charAt(i);
+            if(alpha[str.charAt(i)-97]==0){    //만약 처음 나온 문자라면 알파벳배열의 값을 1로 
+                 alpha[str.charAt(i)-97] = 1; 
+                 check = str.charAt(i); // 연속문자를 확인하기 위해 check 변수에 알파벳 값을 넣어줌
             }
-            if(check == str.charAt(i)){
+            if(check == str.charAt(i)){ //연속된 문자면 넘어감
                 continue;
             }
-            else{
+            else{  // 알파벳 배열의 값도 1이면서 연속된 문자도 아니라면 중복된 문자가 나중에 나온 경우이므로 카운트를 1올려주고 멈춰줌
                 cnt++;
                 break;
             }
         }
-        testCase--;
+        temp--;
     }
-    int sum = temp - cnt ;
+    int sum = testCase - cnt ; //총 단어의 개수 - 중복된 횟수 = 정답!
+    sc.close();
     System.out.println(sum);
         
   }
