6c9a371a2ecedbd397924ee7a6b22d108e84f7ea
Junyong <jypark93@kookmin.ac.kr>
2022-05-08 22:52:29 +0900

    add:week1_BTS

---
+++ datastructure/Month5/week1/binarytree.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+typedef struct TreeNode{
+    element key;
+    struct TreeNode * left;
+    struct TreeNode * right;
+}TreeNode;
+
+TreeNode * insertNode(TreeNode* Root, element key){
+    if(Root == NULL){ // 루트노드가 비어있다면 새로운 노드 생성
+        TreeNode * node = (TreeNode*)malloc(sizeof(TreeNode));
+        node->key = key;
+        node->left = NULL; //자식이 없는 단말노드
+        node->right = NULL; //자식이 없는 단말노드
+        return node; // 만들어진 노드의 주소를 반환
+    } 
+    // 루트가 NULL이 아닌경우 노드가 들어갈 위치를 찾아야 함(재귀적으로)
+
+    if(key < Root->key) //들어올 key값과 root의 key를 비교 같은경우는 아무것도 하지않는다.
+        Root->left = insertNode(Root->left,key);
+    else if(key > Root->key)
+        Root->right = insertNode(Root->right, key);
+    
+    return Root;
+    
+}
+
+
+
+
+int main(){
+
+    TreeNode * Root = NULL;
+    Root = insertNode(Root, 35); //날라오는 주소를 받아줘야한다.
+    Root = insertNode(Root, 68);
+    Root = insertNode(Root, 99);
+    Root = insertNode(Root, 18);
+    Root = insertNode(Root, 7);
+    Root = insertNode(Root, 3);
+    Root = insertNode(Root, 12);
+    Root = insertNode(Root, 26);
+    Root = insertNode(Root, 22);
+    Root = insertNode(Root, 30);
+    return 0;
+}
