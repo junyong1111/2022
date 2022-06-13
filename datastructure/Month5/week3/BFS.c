a14ee0e5f163130e159782319ee6a5fd45ddc928
Junyong <jypark93@kookmin.ac.kr>
2022-06-13 22:21:15 +0900

    add:Grpah_serach

--- datastructure/Month5/week3/BFS.c
+++ datastructure/Month5/week3/BFS.c
@@ -1,254 +1,288 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 10
 #define FALSE 0
 #define TRUE 1
-#define SIZE 10
 
-/* ----------------------------------------------- */
 
-typedef struct{
-    int elem[SIZE];
+typedef struct
+{
+    int stack[N];
     int top;
 }StackType;
 
-
-void initStack(StackType* S){
+void initStack(StackType* S)
+{
     S->top = -1;
 }
 
-
-int isStackEmpty(StackType* S){
-   return S->top == -1;
-}
-
-
-int isStackFull(StackType* S){
-    return S->top == SIZE - 1;
-}
-
-
- 
-void push(StackType* S, int vName)
+int isEmpty(StackType* S)
 {
-    if(isStackFull(S)){
-        printf("Overflow\n");
-        return;
-    }
+    return S->top == -1;
+}
 
-    S->top++;
-    S->elem[S->top] = vName;
+int isFull(StackType* S)
+{
+    return S->top == N - 1;
+}
+
+void push(StackType* S, int e)
+{
+    if(isFull(S))
+        printf("Full\n");
+    else
+    {
+        S->top++;
+        S->stack[S->top] = e;
+    }
 }
 
 
-int pop(StackType* S){
-    if(isStackEmpty(S)){
+int pop(StackType* S)
+{
+    if(isEmpty(S))
+    {
         printf("Empty\n");
-        return 0;
+        return -1;
     }
-  
-    int vName = S->elem[S->top];
+    int elem = S->stack[S->top];
     S->top--;
-    return vName;
+    return elem;
 }
 
-
-int peek(StackType* S){
-    if(isStackEmpty(S)){
+int peek(StackType* S)
+{
+    if(isEmpty(S))
+    {
         printf("Empty\n");
-        return 0;
+        return -1;
     }
-  
-    return S->elem[S->top];
+    int elem = S->stack[S->top];
+    return elem;
 }
+////////////////////////////////////STACK//////////////////////////////////////
 
 
-/* ----------------------------------------------- */
-
-
-typedef struct{
-int elem[SIZE];
+typedef struct
+{
+char elem[N];
 int front, rear;
 }QueueType;
 
-
-void initQueue(QueueType* Q){
+void initQueue(QueueType* Q)
+{
 Q->front = Q->rear = 0;
 }
 
-
-int isQueueEmpty(QueueType* Q){
-
+int isQueueEmpty(QueueType* Q)
+{
 return Q->rear == Q->front;
 }
 
-
- 
-int isQueueFull(QueueType* Q){
-
-return (Q->rear + 1) % SIZE == Q->front;
+int isQueueFull(QueueType* Q)
+{
+return (Q->rear + 1) % N == Q->front;
 }
 
+void enqueue(QueueType* Q, char vName)
+{
+if (isQueueFull(Q))
+{
+printf("FULL\n");
+return;
+}
 
-void enqueue(QueueType* Q, int vName){
-    if (isQueueFull(Q)){
-        printf("FULL\n");
-        return;
-        }
-
-Q->rear = (Q->rear + 1) % SIZE;
+Q->rear = (Q->rear + 1) % N;
 Q->elem[Q->rear] = vName;
 }
 
-
-int dequeue(QueueType* Q){
-
-    if (isQueueEmpty(Q)){
-        printf("EMPTY\n");
-        return 0;
+char dequeue(QueueType* Q)
+{
+if (isQueueEmpty(Q))
+{
+printf("EMPTY\n");
+return 0;
 }
 
-Q->front = (Q->front + 1) % SIZE;
+Q->front = (Q->front + 1) % N;
 return Q->elem[Q->front];
 }
+////////////////////////////////////QUEUE//////////////////////////////////////
 
 
- 
-/* ----------------------------------------------- */
+// 엣지 정보 구조체
+typedef struct IncidentEdge{
+    int Inode;
+    struct IncidentEdge * Inext;
+}IncidentEdge;
 
 
-typedef struct incidentEdge
-{
-    int key;
-    struct incidentEdge * next;
-    
-}incidentEdge; //인접엣지는 연결리스트를 계속 추가해주면 된다.
-
+// 정점 정보 구조체
 typedef struct Vertex{
-    int vKey;
+    int Vnode;
     int isVisit;
-    struct Vertex * next;
-    incidentEdge * iHead;
-}Vertex; 
-//정점 또는 노드의 구성요소
-// 1. 노드의 들어갈 값
-// 2. 노드를 방문했는지 여부
-// 3. 다음 노드를 가르킬 포인터
-// 4. 해당 노드의 인접엣지를 가르킬 포인터
+    struct Vertex * Vnext;
+    struct IncidentEdge * Inext;
+}Vertex;
 
+// 그래프의 헤드
 typedef struct Graph{
-    Vertex * vHead;
+    Vertex * G_head;
 }Graph;
-// 그래프의 연결리스트 표현을 위해 최초 노드
 
+// 초기화
 void init(Graph *G){
-    G->vHead = NULL;
-}
-// 초기 헤드의 포인터를 NULL로 지정
-
-void makeVertex(Graph *G, int key){
-    Vertex * v = (Vertex*)malloc(sizeof(Vertex));
-    v->isVisit = FALSE;
-    v->vKey = key;
-    v->iHead = NULL;
-    v->next = NULL;
-// 노드 추가 시 기본셋팅
-    Vertex * p = G->vHead;
-    if(p==NULL){ //최초의 노드일 경우
-        G->vHead = v;
-    }
-    else{ //최초 노드가 아닐경우
-        while(p->next!=NULL)
-            p = p->next;
-        p->next = v;   
-    }
+    G->G_head = NULL;
 }
 
-void makeIncidentEdge(Vertex *v, int key){
-    incidentEdge * E = (incidentEdge*)malloc(sizeof(incidentEdge));
-    E->key = key;
-    E->next = NULL;
-    // 초기 셋팅
-    incidentEdge * q = v->iHead;
-    // 현재 정점의 인접헤드를 저장
-    if(q==NULL){ // 최초의 인접헤드
-        v->iHead = E;
-    }
-    else{ // 비어있는 인접헤드까지 
-        while(q->next!=NULL)
-            q = q->next;
-        q->next = E;
+// 정점 만들기
+void makeVertex(Graph *G, int Node){
+    Vertex * V = (Vertex*)malloc(sizeof(Vertex));
+
+    V->Vnode = Node;
+    V->isVisit = FALSE;
+    V->Inext = NULL;
+    V->Vnext = NULL;
+
+    Vertex *p = G->G_head;
+
+    if(p==NULL) //최초의 정점
+         G->G_head = V;
+    else{
+        while(p->Vnext!=NULL)
+            p = p->Vnext;
+        p->Vnext = V;
     }
 }
 
-Vertex *findVertex(Graph *G, int vKey){
-    Vertex * v = G->vHead; 
-    while(v->vKey != vKey) //원하는 키까지 간 후
-        v = v->next; 
+Vertex *findVertex(Graph *G, int Node){
+    Vertex * p = G->G_head;
 
-
-    return v; //해당 정점 반환
+    while(p->Vnode != Node)
+        p = p->Vnext;
+    return p;
 }
 
-void insertEdge(Graph *G, int vKey1, int vKey2){
-    Vertex * v = findVertex(G,vKey1); //해당 정점을 찾고
-    makeIncidentEdge(v,vKey2); // 서로 연결
-    v = findVertex(G,vKey2);
-    makeIncidentEdge(v,vKey1);
+void makeIncidentEdge(Vertex *V, int V2){
+    IncidentEdge * E = (IncidentEdge*)malloc(sizeof(IncidentEdge));
+    
+    E->Inode = V2;
+    E->Inext = NULL;
+
+    IncidentEdge * q = V->Inext;
+
+    if(q==NULL)
+        V->Inext = E;
+    else{
+        while(q->Inext !=NULL)
+            q = q->Inext;
+        q->Inext = E;
+    }
+
 }
+
+void insertEdge(Graph *G, int V1, int V2){
+    Vertex *V = findVertex(G,V1);
+    makeIncidentEdge(V, V2);
+    V = findVertex(G,V2);
+    makeIncidentEdge(V, V1);
+}
+
+void BFS(Graph *G, int Target){
+    Vertex *V = findVertex(G, Target);
+    IncidentEdge *E;
+
+    QueueType Q;
+    initQueue(&Q);
+
+    V->isVisit = TRUE;
+    printf("[%d] ", V->Vnode);
+    enqueue(&Q, V->Vnode);
+
+    while(!isQueueEmpty(&Q)){
+        V = findVertex(G, dequeue(&Q));
+        for(E=V->Inext; E!=NULL; E= E->Inext){
+            V =findVertex(G,E->Inode);
+            if(V->isVisit == FALSE){
+                V->isVisit = TRUE;
+                printf("[%d] ", V->Vnode);
+                enqueue(&Q, V->Vnode);
+            }
+        }
+    }
+}
+
+void DFS(Graph *G, int Target){
+    Vertex *V = findVertex(G, Target);
+    IncidentEdge *E;
+
+    StackType S;
+    initStack(&S);
+
+    push(&S, V->Vnode);
+
+    while(!isEmpty(&S)){
+        V = findVertex(G, peek(&S));
+        if(V->isVisit == FALSE){
+            V->isVisit = TRUE;
+            printf("[%d] ", V->Vnode);
+        }
+
+        for(E=V->Inext; E!=NULL; E= E->Inext){
+            V = findVertex(G, E->Inode);
+            if(V->isVisit == FALSE){
+                push(&S, V->Vnode);
+                break;
+            }
+        }
+        if(E==NULL)
+            pop(&S);
+    }
+}
+
+
 
 void print(Graph *G){
-    Vertex * p = G->vHead;
-    incidentEdge * q;
-    for(; p!=NULL; p= p->next){
-        printf("[%d] ",p->vKey);
-        for(q = p->iHead; q!=NULL; q= q->next){
-            printf("[%d] ", q->key);
+    Vertex *p = G->G_head;
+    IncidentEdge *q;
+
+    for(; p!=NULL; p=p->Vnext){
+        printf("[%d] ", p->Vnode);
+        for(q= p->Inext; q!=NULL; q= q->Inext){
+            printf("[%d] ", q->Inode);
         }
         printf("\n");
     }
     printf("\n");
 }
-void BFS(Graph *G, int Target){
-    Vertex *v = findVertex(G, Target);
-    incidentEdge *e;
-    QueueType Q;
-    initQueue(&Q);
 
-    enqueue(&Q, v->vKey);
 
-    while(!isQueueEmpty(&Q)){
-        v = findVertex(G,dequeue(&Q));
-        if(v->isVisit == FALSE){
-            v->isVisit = TRUE;
-            printf("[%d] ", v->vKey); 
-            for(e=v->iHead; e!=NULL; e = e->next){
-                enqueue(&Q, e->key);
-            }
-        }
-    }
-}
-void initVisit(Graph *G, int node){
-    Vertex * v = G->vHead;
-    for(int i=0; i<node; i++){
-        v->isVisit = FALSE;
-        v = v->next;
-    }
-}
+
+
+
 int main(){
-
+    
     Graph G;
-    init(&G);  
+    init(&G);
 
+    
     makeVertex(&G, 1); makeVertex(&G, 2); makeVertex(&G, 3);
-    makeVertex(&G, 4); 
+    makeVertex(&G, 4); makeVertex(&G, 5); makeVertex(&G, 6);
+    makeVertex(&G, 7); makeVertex(&G, 8);
 
-    insertEdge(&G, 1, 2); insertEdge(&G, 1,3);
-    insertEdge(&G, 1,4); insertEdge(&G, 2,4);
-    insertEdge(&G, 3,4); 
+    insertEdge(&G, 1, 2); insertEdge(&G, 1, 3);
+    insertEdge(&G, 2, 4); insertEdge(&G, 2, 5);
+    insertEdge(&G, 3, 5); insertEdge(&G, 5, 6);
+    insertEdge(&G, 6, 7); insertEdge(&G, 7, 8);
 
     print(&G);
-    BFS(&G, 1);
+
+    // DFS(&G,1);
     printf("\n");
+    BFS(&G,1);
+
+    
+    
     return 0;
 }
--- datastructure/Month5/week3/DFS.c
+++ datastructure/Month5/week3/DFS.c
@@ -1,256 +1,217 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 10
 #define FALSE 0
 #define TRUE 1
-#define SIZE 10
 
-/* ----------------------------------------------- */
 
-typedef struct{
-    int elem[SIZE];
+typedef struct
+{
+    int stack[N];
     int top;
 }StackType;
 
-
-void initStack(StackType* S){
+void initStack(StackType* S)
+{
     S->top = -1;
 }
 
-
-int isStackEmpty(StackType* S){
-   return S->top == -1;
-}
-
-
-int isStackFull(StackType* S){
-    return S->top == SIZE - 1;
-}
-
-
- 
-void push(StackType* S, int vName)
+int isEmpty(StackType* S)
 {
-    if(isStackFull(S)){
-        printf("Overflow\n");
-        return;
-    }
+    return S->top == -1;
+}
 
-    S->top++;
-    S->elem[S->top] = vName;
+int isFull(StackType* S)
+{
+    return S->top == N - 1;
+}
+
+void push(StackType* S, int e)
+{
+    if(isFull(S))
+        printf("Full\n");
+    else
+    {
+        S->top++;
+        S->stack[S->top] = e;
+    }
 }
 
 
-int pop(StackType* S){
-    if(isStackEmpty(S)){
+int pop(StackType* S)
+{
+    if(isEmpty(S))
+    {
         printf("Empty\n");
-        return 0;
+        return -1;
     }
-  
-    int vName = S->elem[S->top];
+    int elem = S->stack[S->top];
     S->top--;
-    return vName;
+    return elem;
 }
 
-
-int peek(StackType* S){
-    if(isStackEmpty(S)){
+int peek(StackType* S)
+{
+    if(isEmpty(S))
+    {
         printf("Empty\n");
-        return 0;
+        return -1;
     }
-  
-    return S->elem[S->top];
+    int elem = S->stack[S->top];
+    return elem;
+}
+////////////////////////////////////STACK//////////////////////////////////////
+
+// 엣지 정보 구조체
+typedef struct IncidentEdge{
+    int Inode;
+    struct IncidentEdge * Inext;
+}IncidentEdge;
+
+
+// 정점 정보 구조체
+typedef struct Vertex{
+    int Vnode;
+    int isVisit;
+    struct Vertex * Vnext;
+    struct IncidentEdge * Inext;
+}Vertex;
+
+// 그래프의 헤드
+typedef struct Graph{
+    Vertex * G_head;
+}Graph;
+
+// 초기화
+void init(Graph *G){
+    G->G_head = NULL;
+}
+
+// 정점 만들기
+void makeVertex(Graph *G, int Node){
+    Vertex * V = (Vertex*)malloc(sizeof(Vertex));
+
+    V->Vnode = Node;
+    V->isVisit = FALSE;
+    V->Inext = NULL;
+    V->Vnext = NULL;
+
+    Vertex *p = G->G_head;
+
+    if(p==NULL) //최초의 정점
+         G->G_head = V;
+    else{
+        while(p->Vnext!=NULL)
+            p = p->Vnext;
+        p->Vnext = V;
+    }
+}
+
+Vertex *findVertex(Graph *G, int Node){
+    Vertex * p = G->G_head;
+
+    while(p->Vnode != Node)
+        p = p->Vnext;
+    return p;
+}
+
+void makeIncidentEdge(Vertex *V, int V2){
+    IncidentEdge * E = (IncidentEdge*)malloc(sizeof(IncidentEdge));
+    
+    E->Inode = V2;
+    E->Inext = NULL;
+
+    IncidentEdge * q = V->Inext;
+
+    if(q==NULL)
+        V->Inext = E;
+    else{
+        while(q->Inext !=NULL)
+            q = q->Inext;
+        q->Inext = E;
+    }
+
+}
+
+void insertEdge(Graph *G, int V1, int V2){
+    Vertex *V = findVertex(G,V1);
+    makeIncidentEdge(V, V2);
+    V = findVertex(G,V2);
+    makeIncidentEdge(V, V1);
 }
 
 
-/* ----------------------------------------------- */
+void DFS(Graph *G, int Target){
+    // Step1.시작 정점을 찾는다
+    Vertex *V = findVertex(G, Target);
+    IncidentEdge *E;
+    // Step2.스택 생성 및 초기화
+    StackType S;
+    initStack(&S);
 
-
-typedef struct{
-int elem[SIZE];
-int front, rear;
-}QueueType;
-
-
-void initQueue(QueueType* Q){
-Q->front = Q->rear = 0;
-}
-
-
-int isQueueEmpty(QueueType* Q){
-
-return Q->rear == Q->front;
-}
-
-
- 
-int isQueueFull(QueueType* Q){
-
-return (Q->rear + 1) % SIZE == Q->front;
-}
-
-
-void enqueue(QueueType* Q, int vName){
-    if (isQueueFull(Q)){
-        printf("FULL\n");
-        return;
+    // Step3.첫 정점 푸쉬
+    push(&S, V->Vnode);
+    // Step4. 스택의 원소가 없을때까지
+    while(!isEmpty(&S)){
+        // 스택의 최상단의 정점을 찾음 -> 방문체크 -> 출력 -> 인접노드 확인 방문하지않은 노드가 나오면 push -> break;
+        V = findVertex(G, peek(&S));
+        if(V->isVisit == FALSE){
+            V->isVisit = TRUE;
+            printf("[%d] ", V->Vnode);
         }
 
-Q->rear = (Q->rear + 1) % SIZE;
-Q->elem[Q->rear] = vName;
-}
-
-
-int dequeue(QueueType* Q){
-
-    if (isQueueEmpty(Q)){
-        printf("EMPTY\n");
-        return 0;
-}
-
-Q->front = (Q->front + 1) % SIZE;
-return Q->elem[Q->front];
-}
-
-
- 
-/* ----------------------------------------------- */
-
-
-typedef struct incidentEdge
-{
-    int key;
-    struct incidentEdge * next;
-    
-}incidentEdge; //인접엣지는 연결리스트를 계속 추가해주면 된다.
-
-typedef struct Vertex{
-    int vKey;
-    int isVisit;
-    struct Vertex * next;
-    incidentEdge * iHead;
-}Vertex; 
-//정점 또는 노드의 구성요소
-// 1. 노드의 들어갈 값
-// 2. 노드를 방문했는지 여부
-// 3. 다음 노드를 가르킬 포인터
-// 4. 해당 노드의 인접엣지를 가르킬 포인터
-
-typedef struct Graph{
-    Vertex * vHead;
-}Graph;
-// 그래프의 연결리스트 표현을 위해 최초 노드
-
-void init(Graph *G){
-    G->vHead = NULL;
-}
-// 초기 헤드의 포인터를 NULL로 지정
-
-void makeVertex(Graph *G, int key){
-    Vertex * v = (Vertex*)malloc(sizeof(Vertex));
-    v->isVisit = FALSE;
-    v->vKey = key;
-    v->iHead = NULL;
-    v->next = NULL;
-// 노드 추가 시 기본셋팅
-    Vertex * p = G->vHead;
-    if(p==NULL){ //최초의 노드일 경우
-        G->vHead = v;
+        for(E=V->Inext; E!=NULL; E= E->Inext){
+            V = findVertex(G, E->Inode);
+            if(V->isVisit == FALSE){
+                push(&S, V->Vnode);
+                break;
+            }
+        }
+        // Step5. 만약 인접노드를 다 방문 했다면 해당 노드 삭제
+        if(E==NULL)
+            pop(&S);
     }
-    else{ //최초 노드가 아닐경우
-        while(p->next!=NULL)
-            p = p->next;
-        p->next = v;   
-    }
-}
-
-void makeIncidentEdge(Vertex *v, int key){
-    incidentEdge * E = (incidentEdge*)malloc(sizeof(incidentEdge));
-    E->key = key;
-    E->next = NULL;
-    // 초기 셋팅
-    incidentEdge * q = v->iHead;
-    // 현재 정점의 인접헤드를 저장
-    if(q==NULL){ // 최초의 인접헤드
-        v->iHead = E;
-    }
-    else{ // 비어있는 인접헤드까지 
-        while(q->next!=NULL)
-            q = q->next;
-        q->next = E;
-    }
-}
-
-Vertex *findVertex(Graph *G, int vKey){
-    Vertex * v = G->vHead; 
-    while(v->vKey != vKey) //원하는 키까지 간 후
-        v = v->next; 
-
-
-    return v; //해당 정점 반환
-}
-
-void insertEdge(Graph *G, int vKey1, int vKey2){
-    Vertex * v = findVertex(G,vKey1); //해당 정점을 찾고
-    makeIncidentEdge(v,vKey2); // 서로 연결
-    v = findVertex(G,vKey2);
-    makeIncidentEdge(v,vKey1);
 }
 
 void print(Graph *G){
-    Vertex * p = G->vHead;
-    incidentEdge * q;
-    for(; p!=NULL; p= p->next){
-        printf("[%d] ",p->vKey);
-        for(q = p->iHead; q!=NULL; q= q->next){
-            printf("[%d] ", q->key);
+    Vertex *p = G->G_head;
+    IncidentEdge *q;
+
+    for(; p!=NULL; p=p->Vnext){
+        printf("[%d] ", p->Vnode);
+        for(q= p->Inext; q!=NULL; q= q->Inext){
+            printf("[%d] ", q->Inode);
         }
         printf("\n");
     }
     printf("\n");
 }
 
-void DFS(Graph *G, int Target){
-    Vertex *v = findVertex(G, Target);
-    incidentEdge *q;
-    StackType S;
-    initStack(&S);
-    push(&S, v->vKey);
-    while(!isStackEmpty(&S)){
-        v = findVertex(G,peek(&S));
-            if(v->isVisit == FALSE){
-                v->isVisit = TRUE;
-                printf("[%d] ", v->vKey); 
-            }
-            for(q=v->iHead; q!=NULL; q = q->next){
-                v = findVertex(G, q->key);
-                {
-                    if(v->isVisit == FALSE){
-                        push(&S, v->vKey);
-                        break;
-                    }
-                }
-        }
-        if(q==NULL){
-            pop(&S);
-        }
-    }
-}
+
+
 
 
 int main(){
+    
     Graph G;
-    init(&G);  
+    init(&G);
 
+    
     makeVertex(&G, 1); makeVertex(&G, 2); makeVertex(&G, 3);
-    makeVertex(&G, 4); 
+    makeVertex(&G, 4); makeVertex(&G, 5); makeVertex(&G, 6);
+    makeVertex(&G, 7); makeVertex(&G, 8);
 
-    insertEdge(&G, 1, 2); insertEdge(&G, 1,3);
-    insertEdge(&G, 1,4); insertEdge(&G, 2,4);
-    insertEdge(&G, 3,4); 
+    insertEdge(&G, 1, 2); insertEdge(&G, 1, 3);
+    insertEdge(&G, 2, 4); insertEdge(&G, 2, 5);
+    insertEdge(&G, 3, 5); insertEdge(&G, 5, 6);
+    insertEdge(&G, 6, 7); insertEdge(&G, 7, 8);
 
     print(&G);
-    DFS(&G, 1);
-    printf("\n");
+
+    DFS(&G,1);
+
+    
+    
     return 0;
 }
