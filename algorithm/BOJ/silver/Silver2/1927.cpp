c032a8f66715fc7dae30303d2afbdf9e0dcb90b8
Junyong <jypark93@kookmin.ac.kr>
2022-07-16 22:06:15 +0900

    add:algorithm_Search

--- algorithm/BOJ/silver/Silver2/1927.cpp
+++ algorithm/BOJ/silver/Silver2/1927.cpp
@@ -13,7 +13,7 @@ int main(){
     // freopen("input.txt", "r", stdin);
 
     priority_queue<int, vector<int> , greater<int> >pq;
-  
+    //오름차순 우선순위 큐 새성
 
     int testCase = 0;
     cin >> testCase;
@@ -21,13 +21,16 @@ int main(){
         int Num = 0;
         cin >> Num;
         if(Num!=0)
-            pq.push(Num);
+            pq.push(Num); 
+            //0이 아니라면 푸쉬
         else{
             if(pq.empty())
                 cout << 0 <<"\n";
+                // 0인데 원소가 비어있다면 0 출력
             else{
                 cout << pq.top() <<"\n";
                 pq.pop();
+                // 0이면서 원소가 있다면 제일 작은 값 반환
             }
         }
     }
