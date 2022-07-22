bcbde9abb1573835dfe865a4bcc3ceb442c900e4
Junyong <jypark93@kookmin.ac.kr>
2022-07-22 20:47:16 +0900

    fix:algo_pro_levle2

--- algorithm/programmers/Stack:Queue/올바른괄호.cpp
+++ algorithm/programmers/Stack:Queue/올바른괄호.cpp
@@ -1,36 +1,27 @@
+#include <string>
 #include <iostream>
-#include <vector>
+#include <stack>
 #include <algorithm>
-
-#define A 4
-#define B 2
 
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    // freopen("input.txt" , "r", stdin);
-
-    vector<int>ScoreA(A,0);
-    vector<int>ScoreB(B,0);
-    for(int i=0; i<A; i++)
-        cin >> ScoreA[i] ;
-    for(int i=0; i<B; i++)
-        cin >> ScoreB[i] ;
-    sort(ScoreA.rbegin(), ScoreA.rend());
-    sort(ScoreB.rbegin(), ScoreB.rend());
-    int sum = 0;
-    for(int i=0; i<A-1; i++){
-        sum = sum + ScoreA[i];
-    }
-    for(int i=0; i<B-1; i++){
-        sum = sum + ScoreB[i];
-    }
+bool solution(string s)
+{
+    bool answer = true;
+    int len = s.length();
+    stack<char>S;
+    for(int i=0; i<len; i++){
+        if(s[i] =='(')
+            S.push(s[i]);
+        
+        else if(s[i] == ')'){
+            if(S.empty()){answer = false; return answer;}
+            S.pop();
+        }
+    }//for
     
-    cout << sum << "\n";
-
-    return 0;
+    if(!S.empty())
+        answer = false;
+    
+    return answer;
 }
