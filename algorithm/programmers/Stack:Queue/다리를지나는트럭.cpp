4d58bc6d0371d2d0f5a0dcb514418001867528c0
Junyong <jypark93@kookmin.ac.kr>
2022-07-27 21:33:02 +0900

    add:algo_pro_2_stack_queue

---
+++ algorithm/programmers/Stack:Queue/다리를지나는트럭.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 1;
+    
+    int len = bridge_length;
+    
+    
+    
+    for(int i=0; i<truck_weights.size()-1; i++){
+        int temp = truck_weights[i] + truck_weights[i+1];
+        if(temp <= weight){
+            answer ++;
+        }
+        else{
+            answer = answer + len;
+        }
+    }
+    
+    answer = answer + len;
+    
+    
+    return answer;
+}
