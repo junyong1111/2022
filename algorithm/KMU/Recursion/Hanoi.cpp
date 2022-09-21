1e61a3b942ed4c080e6c6a71a2375536cba1c606
Junyong <jypark93@kookmin.ac.kr>
2022-09-21 22:57:09 +0900

    add:algorithm_kmu

---
+++ algorithm/KMU/Recursion/Hanoi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+
+    return 0;
+}
