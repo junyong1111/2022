b0cfd9bdea8fea874c5233bb7cc094fbfaf3077f
Junyong <jypark93@kookmin.ac.kr>
2022-11-13 21:29:45 +0900

    add:algorithm_LCS_R

--- algorithm/KMU/Recursion/Longest_Recursion.cpp
+++ algorithm/KMU/Recursion/Longest_Recursion.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <string>
+#define MAX_SIZE 11
+#define MAX(a,b) ((a)>(b)?(a):(b))
 
 using namespace std;
 
@@ -10,18 +11,18 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    freopen("input.txt", "r", stdin);
+    // freopen("input.txt", "r", stdin);
 
     int testCase = 0;
     cin >> testCase;
     
     while(testCase--){
-        char str1[10];
-        char str2[10];
+        char str1[MAX_SIZE];
+        char str2[MAX_SIZE];
         cin >> str1 >> str2;
         int M = Len(str1);
         int N = Len(str2);
-        int answer = LCS_Recursion(str1, str2, M-1,N-1);
+        int answer = LCS_Recursion(str1, str2, M,N);
         cout << answer << "\n";
     }
 
@@ -32,16 +33,17 @@ int LCS_Recursion(char *str1, char *str2, int M, int N){
     if(M==0 || N==0)
         return 0;
     else{
-        if((M>0 && N>0) && (str1[M]) == (str2[N]))
-            return LCS_Recursion(str1,str2, M-1, N-1);
-        else {
-            
+        if((M>0 && N>0) && (str1[M-1]) == (str2[N-1]))
+            return (LCS_Recursion(str1,str2, M-1, N-1))+1;
+        else if((M>0 && N>0) && (str1[M-1]) != (str2[N-1])) {
+            int left = LCS_Recursion(str1,str2,M,N-1);
+            int right = LCS_Recursion(str1,str2,M-1,N);
+            int max = MAX(left, right);
+            return max;
         }
-        
+        else
+            return 0;
     }
-    
-    
-    
 }
 
 int Len(char *str){
